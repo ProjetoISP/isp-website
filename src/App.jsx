80bf36d22e84f14db39813a74a07da72f7e3d761
nicole <nicole@kenzie.com.br>
2023-02-07 09:02:25 -0300

    feat:criando GalleryPage e ProjectsPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import GlobalStyle from "./styles/global";
 import { Header } from "./components/Header/Header";
 import { PresentationPage } from "./pages/PresentationPage/PresentationPage";
 import { WhoWeArePage } from "./pages/WhoWeArePage/WhoWeArePage";
+import Footer from "./components/Footer/Footer";
+import { GalleryPage } from "./pages/GalleryPage/GalleryPage";
+import { ProjectsPage } from "./pages/ProjectsPage/ProjectsPage";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <GlobalStyle />
       <Header />
       <PresentationPage />
       <WhoWeArePage />
+      <ProjectsPage />
+      <GalleryPage />
+      <Footer />
     </div>
   );
 }
