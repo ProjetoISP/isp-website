973d3772ab7bf6d029f5ba26930fc0be45a1fbd3
ProjetoISP <119977739+ProjetoISP@users.noreply.github.com>
2023-02-07 14:43:59 -0300

    feat:criando GalleryPage e ProjectsPage (#1)
    Co-authored-by: nicole <nicole@kenzie.com.br>

---
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -0,0 +1,10 @@
+import { Partners } from "../../components/Partners/Partners";
+
+export const GalleryPage = () => {
+  return (
+    <div>
+      GalleryPage
+      <Partners />
+    </div>
+  );
+};
