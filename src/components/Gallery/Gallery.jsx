397817eef0c1aad33dc59bd31646ea7756ce1c06
josuel mroczko <josuelbeserramroczko@outlook.com>
2023-02-09 11:59:04 -0300

    carrocel da galeria

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import './style.css'
+const images = [
+  imagem1,
+  imagem2,
+  imagem3,
+  imagem4,
+  imagem2,
+  imagem1
+];
 
-export const Gallery = () => {
-  return <div>Gallery</div>;
+
+export const Gallery1 = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex(i => (i + 3) % images.length);
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handlePrevClick = () => {
+    setCurrentIndex(i => (i + images.length - 3) % images.length);
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex(i => (i + 3) % images.length);
+  };
+
+  return <div>
+    <div id="slider">
+        <button onClick={handlePrevClick}>Prev</button>
+        <div className="image-container">
+          {images.slice(currentIndex, currentIndex + 3).map((image, index) => (
+            <img key={index} src={image} alt={`Image ${index + 1}`} />
+          ))}
+        </div>
+        <button onClick={handleNextClick}>Next</button>
+      </div>
+    
+    </div>;
 };
