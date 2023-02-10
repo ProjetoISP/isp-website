7a3a0ff1a5bea2cddf1c31db1f5bb3301f757104
Nicole Pimenta <niipimenta@hotmail.com>
2023-02-10 20:06:22 -0300

    Update who (#10)
    * feat: inserindo componente Gallery
    
    * feat: footer
    
    * fix:merge
    
    * Ajustes na versão mobile, tablet e desktop. Utilizei classes CSS - talvez seja necessário refatorar e passar para styled-component.
    
    * fix:largura video
    
    ---------
    
    Co-authored-by: nicole <nicole@kenzie.com.br>
    Co-authored-by: BrunoSilvaFaria <fariabruno443@gmail.com>

--- src/pages/WhoWeArePage/WhoWeArePage.jsx
+++ src/pages/WhoWeArePage/WhoWeArePage.jsx
@@ -13,25 +13,52 @@ export const WhoWeArePage = () => {
   return (
     <MainContainer id="whoWeAre">
       <FirstSection>
-        <div className="video-frame">
-          <video></video>
-        </div>
-        <div>
-          <h2>QUEM SOMOS</h2>
-          <p>
-            {" "}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet rem
-            temporibus facilis non aliquam sunt veritatis? Pariatur modi at
-            placeat nihil, fuga veniam quas nam culpa vero harum? Facilis,
-            nesciunt?
-          </p>
-          <div className="span-container">
-            <span>10 anos de historia</span>
-            <span>xxx atendimentos por semana</span>
-            <span>150 + familias alcançadas</span>
+        <section className="hero__background">
+          <div className="hero">
+            <section className="section__empresa">
+              <section className="section__quem">
+                <h1 id="projeto__nome" className="titulo__empresa">Quem somos?</h1>
+                <p className="paragrafo paragrafo__empresa">
+                  Somos uma organização não-governamental com sede em Duque de Caxias-RJ.
+                  Atualmente a instituição é formada por uma equipe de mais de 50 voluntários, das
+                  áreas de RH, Pedagogia, Publicidade e Marketing e Tecnologia.
+                  Temos o programa Trilha de Carreira, que visa o desenvolvimento pessoal e
+                  profissional através do nosso ambiente de voluntariado, funcionando como um
+                  laboratório de experiências profissionais.
+                </p>
+              </section>
+              <section className="empresa__info">
+                <div className="circle__info">
+                  <p className="circle__paragrafo--wrap">
+                    10 anos
+                    de <br />
+                    história
+                  </p>
+                </div>
+
+                <div className="circle__info">
+                  <p className="circle__paragrafo">
+                    XXX
+                    atendimentos
+                    por semana
+                  </p>
+                </div>
+
+                <div className="circle__info">
+                  <p className="circle__paragrafo--wrap">
+                    150+
+                    famílias
+                    alcançadas
+                  </p>
+                </div>
+              </section>
+
+            </section>
+            <iframe src="https://www.youtube.com/embed/wTfmgB3mi-4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
-        </div>
+        </section>
       </FirstSection>
+
       <SecondSection>
         <div>
           <h2>Missão</h2>
