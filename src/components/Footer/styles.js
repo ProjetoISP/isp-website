312a623a3df37bc3da52b64d0cc0087674ba420c
Alysson Leite <alysson_leite@yahoo.com.br>
2023-02-14 21:43:31 -0300

    Alterando componentes do Footer e responsividade

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,24 +1,32 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
-  background: linear-gradient(
+  background-image: linear-gradient(
     180deg,
     #63b2bb 34.11%,
     rgba(224, 125, 37, 0.58) 100%
   );
   height: 570px;
 
+  @media(max-width: 425px){
+      min-height: 680px;
+      width: 100%;
+  }
+
   h2 {
     height: 63px;
-    width: 178px;
+    width: 100%;
     margin: auto;
     font-family: Poppins;
     font-size: 42px;
     font-weight: 800;
-    line-height: 63px;
-    text-align: left;
+    text-align: center;
     color: #ffffff;
     padding: 20px;
+
+    @media(max-width: 425px){
+      font-size: 28px;
+    }
   }
 
   .flex {
@@ -29,11 +37,32 @@ export const FooterContainer = styled.div`
     margin: auto;
     height: 80%;
     padding: 10px;
+
+    @media(max-width: 425px){
+      flex-direction: column;
+      justify-content: center;
+    }
   }
+
   p {
-    width: 30%;
+    width: 35%;
+    font-size: 18px;
     margin: 20px auto;
+
+    @media(max-width: 768px){
+        font-size: 10.5px;
+    }
+
+    @media(max-width: 425px){
+        font-size: 8px;
+        width: 50%;
+        margin: 60px auto 0 auto;
+    }
   }
+
+  @media only screen and (min-width: 768px) {
+        min-height: 340px;
+    }
 `;
 
 export const FormContainer = styled.div`
@@ -41,16 +70,25 @@ export const FormContainer = styled.div`
   min-width: 450px;
   width: 45%;
 
+  @media(max-width: 425px){
+        margin: 30px 0 30px 0;
+    }
+
   p {
     height: 28px;
     width: 189px;
     font-family: Roboto;
     font-size: 24px;
     font-weight: 400;
-    line-height: 28px;
     text-align: left;
-    margin: 20px auto;
+    margin: 20px auto 10px auto;
     color: #1e1e1e;
+
+    @media(max-width: 425px){
+        font-size: 16px;
+        text-align: center;
+        margin: 40px auto 0 auto;
+    }
   }
 `;
 
@@ -60,37 +98,84 @@ export const MapContainer = styled.div`
   width: 45%;
   display: flex;
   flex-direction: column;
+  margin-top: 20px;
+`;
 
-  img {
-    height: 168px;
-    width: 390px;
+export const Maps = styled.div`
     margin: auto;
-  }
+    width: 390px;
+    height: 168px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-  p {
-    width: 62%;
-    margin: 10px auto;
+    @media (max-width: 425px) {
+        width: 280px;
+        height: 100px;
+    }
+
+    iframe {
+        width: 390px;
+        border: 0;
+
+        @media (max-width: 425px) {
+        width: 280px;
+    }
+    }
+`
+
+export const MapText = styled.div`
+    width: 390px;
+    margin-top: 15px;
+    margin-left: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    @media (max-width: 425px) {
+        width: 280px;
+        height: 100px;
+        margin: 30px 0 10px 85px;
+    }
+
+    p {
     font-family: Roboto;
+    width: 50%;
+    margin: 0;
     font-size: 18px;
-    font-weight: 400;
-    line-height: 21px;
-    text-align: left;
     color: #1e1e1e;
+    
+      @media (max-width: 425px){
+          font-size: 12px;
+      }
   }
-`;
+`
 
+/* Ícones das Redes Sociais */
 export const IconContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 42px;
-  width: 62%;
-  margin: auto;
+    width: 390px;
+    margin: 30px auto 0px auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 24px;
 
-  img {
-    height: 40px;
-    width: 54px;
-    left: 786px;
-    margin: 10px;
-  }
-`;
+    @media (max-width: 425px) {
+      margin: -50px auto 0 auto;
+      gap: 14px;
+    }
+`
+
+export const SocialIcons = styled.a`
+    color: #F5F5F5;
+    font-size: 48px;
+    transition: .3s ease-in-out;
+
+    &:hover {
+        transform: scale(1.3);
+    }
+
+    @media (max-width: 425px) {
+        font-size: 32px;
+    }
+`
