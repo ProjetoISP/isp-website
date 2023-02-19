47dfb3f76b8c8ee5e3dc5f420391768bbb8a0350
nicole <nicole@kenzie.com.br>
2023-02-19 14:26:06 -0300

    feat: responsividade footer e parceiros

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,24 +1,32 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
-  background: linear-gradient(
+  background-image: linear-gradient(
     180deg,
     #63b2bb 34.11%,
     rgba(224, 125, 37, 0.58) 100%
   );
   height: 570px;
 
+  @media (max-width: 425px) {
+    min-height: 680px;
+    width: 100%;
+  }
+
   h2 {
     height: 63px;
-    width: 178px;
+    width: 100%;
     margin: auto;
     font-family: Poppins;
     font-size: 42px;
     font-weight: 800;
-    line-height: 63px;
-    text-align: left;
+    text-align: center;
     color: #ffffff;
-    padding: 20px;
+    padding: 10px;
+
+    @media (max-width: 425px) {
+      font-size: 28px;
+    }
   }
 
   .flex {
@@ -29,17 +37,45 @@ export const FooterContainer = styled.div`
     margin: auto;
     height: 80%;
     padding: 10px;
+
+    @media (max-width: 425px) {
+      flex-direction: column;
+      justify-content: center;
+    }
   }
+
   p {
-    width: 30%;
+    width: 35%;
+    font-size: 18px;
     margin: 20px auto;
+
+    @media (max-width: 768px) {
+      font-size: 10.5px;
+    }
+
+    @media (max-width: 425px) {
+      font-size: 8px;
+      width: 50%;
+      margin: 60px auto 0 auto;
+    }
+  }
+
+  @media only screen and (min-width: 768px) {
+    min-height: 340px;
   }
 `;
 
 export const FormContainer = styled.div`
-  height: 95%;
+  height: 100%;
   min-width: 450px;
   width: 45%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 425px) {
+    margin: 30px 0 30px 0;
+  }
 
   p {
     height: 28px;
@@ -47,10 +83,15 @@ export const FormContainer = styled.div`
     font-family: Roboto;
     font-size: 24px;
     font-weight: 400;
-    line-height: 28px;
     text-align: left;
-    margin: 20px auto;
+    margin: 10px auto;
     color: #1e1e1e;
+
+    @media (max-width: 425px) {
+      font-size: 16px;
+      text-align: center;
+      margin: 40px auto 0 auto;
+    }
   }
 `;
 
@@ -60,37 +101,84 @@ export const MapContainer = styled.div`
   width: 45%;
   display: flex;
   flex-direction: column;
+  margin-top: 20px;
+`;
 
-  img {
-    height: 168px;
-    width: 390px;
-    margin: auto;
+export const Maps = styled.div`
+  margin: auto;
+  width: 390px;
+  height: 168px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 425px) {
+    width: 280px;
+    height: 100px;
   }
 
-  p {
-    width: 62%;
-    margin: 10px auto;
-    font-family: Roboto;
-    font-size: 18px;
-    font-weight: 400;
-    line-height: 21px;
-    text-align: left;
-    color: #1e1e1e;
+  iframe {
+    width: 390px;
+    border: 0;
+
+    @media (max-width: 425px) {
+      width: 280px;
+    }
   }
 `;
 
-export const IconContainer = styled.div`
+export const MapText = styled.div`
+  width: 390px;
+  margin-top: 15px;
+  margin-left: 30px;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 42px;
-  width: 62%;
-  margin: auto;
+  flex-direction: column;
+  align-items: flex-start;
 
-  img {
-    height: 40px;
-    width: 54px;
-    left: 786px;
-    margin: 10px;
+  @media (max-width: 425px) {
+    width: 280px;
+    height: 100px;
+    margin: 30px 0 10px 85px;
+  }
+
+  p {
+    font-family: Roboto;
+    width: 50%;
+    margin: 0;
+    font-size: 18px;
+    color: #1e1e1e;
+
+    @media (max-width: 425px) {
+      font-size: 12px;
+    }
+  }
+`;
+
+/* Ícones das Redes Sociais */
+export const IconContainer = styled.div`
+  width: 390px;
+  margin: 30px auto 0px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+
+  @media (max-width: 425px) {
+    margin: -50px auto 0 auto;
+    gap: 14px;
+  }
+`;
+
+export const SocialIcons = styled.a`
+  color: #f5f5f5;
+  font-size: 48px;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.3);
+  }
+
+  @media (max-width: 425px) {
+    font-size: 32px;
   }
 `;
--- src/components/Partners/styles.js
+++ src/components/Partners/styles.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export const PartnersContainer = styled.div`
-  height: 700px;
+  height: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 70vw;
-  margin: auto;
+  flex-wrap: wrap;
+  width: 80vw;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    height: 415px;
+    margin: 20px auto 0px auto;
+  }
 
   h2 {
     height: 69px;
@@ -17,22 +24,61 @@ export const PartnersContainer = styled.div`
     line-height: 69px;
     text-align: left;
     color: #fe6c05;
-    margin: 15px auto;
+    margin: 20px auto;
+
+    @media (max-width: 768px) {
+      margin: 20px auto 0px auto;
+      font-size: 36px;
+    }
+
+    @media (max-width: 425px) {
+      font-size: 28px;
+    }
+  }
+`;
+
+export const CarouselPartners = styled(motion.div)`
+  margin: 40px auto;
+  width: 100%;
+  max-width: 1000px;
+  overflow: hidden;
+
+  @media (max-width: 768px) {
+    cursor: grab;
+    margin-top: 60px;
+    max-width: 680px;
   }
 
-  div {
-    display: flex;
-    margin: auto;
-    width: 70%;
-    height: 60%;
-    align-items: center;
-    justify-content: center;
+  @media (max-width: 992px) {
+    max-width: 800px;
+  }
+
+  @media (max-width: 425px) {
+    max-width: 340px;
+  }
+`;
+
+export const Inner = styled(motion.div)`
+  display: flex;
+
+  @media (min-width: 1200px) {
     flex-wrap: wrap;
+    justify-content: center;
+  }
+`;
+
+export const Item = styled(motion.div)`
+  min-height: 130px;
+  min-width: 130px;
+  padding: 10px;
+
+  @media (min-width: 1200px) {
+    min-width: 160px;
   }
 
   img {
-    height: 138px;
-    width: 158px;
-    margin: 20px;
+    width: 100%;
+    height: 90%;
+    pointer-events: none;
   }
 `;
